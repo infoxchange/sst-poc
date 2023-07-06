aceabc8a2845e6e1d986e9c1a5cf099466e444e3
Callum Gare <cgare@infoxchange.org>
2023-07-06 21:08:34 +1000

    draft

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,10 @@
 import { SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+
+import { Vpc, SubnetType } from "aws-cdk-lib/aws-ec2";
+
 
 export default {
   config(_input) {
@@ -11,7 +15,22 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
+
+      const vpc = Vpc.fromLookup(stack, "myVPC", { vpcId: "vpc-0449d00f66f3c503d" });
+      const vpcSubnets = {
+        subnetType: SubnetType.PRIVATE_ISOLATED,
+      };
       const site = new NextjsSite(stack, "site", {
+        cdk: {
+          server: {
+            vpc,
+            vpcSubnets,
+          },
+          revalidation: {
+            vpc,
+            vpcSubnets,
+          }
+        },
         customDomain: {
           domainName: "sst-poc.cals.cafe",
           isExternalDomain: true,
